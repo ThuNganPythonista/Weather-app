e270ab77e8e4758606385c059c2393d03220c99f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-17 17:49:31 +0700

    third  commit

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const APP_ID = "36dadc6253816027f46d1f8048c05627";
-const searchInput = document.querySelector("#search-input");
+// const APP_ID = "36dadc6253816027f46d1f8048c05627";
+// const searchInput = document.querySelector("#search-input");
 
-searchInput.addEventListener("change", (e) => {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}`
-  ).then(async (res) => {
-    const data = await res.json();
-    console.log("[Search Input]", data);
-  });
+// searchInput.addEventListener("change", (e) => {
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}`
+//   ).then(async (res) => {
+//     const data = await res.json();
+//     console.log("[Search Input]", data);
+//   });
+// });
+document.addEventListener("DOMContentLoaded", function () {
+  const APP_ID = "36dadc6253816027f46d1f8048c05627";
+  const searchInput = document.querySelector("#search-input");
+  const cityName = document.querySelector("city-name");
+  const weatherState = document.querySelector(".weather-state");
+  const temperature = document.querySelector(".temperature");
+  const sunrise = document.querySelector(".sunrise");
+  const sunset = document.querySelector(".sunset");
+  const humidity = document.querySelector(".humidity");
+  const windSpeed = document.querySelector(".wind-speed");
+  const DEFAULT_VALUE = "Not found";
+
+  if (searchInput) {
+    searchInput.addEventListener("change", (e) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&lang=vi`
+      ).then(async (res) => {
+        const data = await res.json();
+        console.log("[Search Input]", data);
+        cityName.innerHTML = data.name || DEFAULT_VALUE;
+        weatherIcon.setAttribute(
+          "src",
+          `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        );
+        temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+        sunset.innerHTML =
+          moment.unix(data.sys.sunset).format("H:mm") || DEFAULT_VALUE;
+        humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
+        windSpeed.innerHTML =
+          (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE; // đổi m/s sang km/h
+      });
+    });
+  } else {
+    console.log("Search input element not found");
+  }
 });
