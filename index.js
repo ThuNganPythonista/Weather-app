700cbf7c31b77cfc3970c8d20de6de4ff1bc17f3
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-05-18 11:23:17 +0700

    4  commit

--- index.js
+++ index.js
@@ -9,23 +9,25 @@
 //     console.log("[Search Input]", data);
 //   });
 // });
-document.addEventListener("DOMContentLoaded", function () {
-  const APP_ID = "36dadc6253816027f46d1f8048c05627";
-  const searchInput = document.querySelector("#search-input");
-  const cityName = document.querySelector("city-name");
-  const weatherState = document.querySelector(".weather-state");
-  const temperature = document.querySelector(".temperature");
-  const sunrise = document.querySelector(".sunrise");
-  const sunset = document.querySelector(".sunset");
-  const humidity = document.querySelector(".humidity");
-  const windSpeed = document.querySelector(".wind-speed");
-  const DEFAULT_VALUE = "Not found";
+// document.addEventListener("DOMContentLoaded", function () {
+const APP_ID = "36dadc6253816027f46d1f8048c05627";
+const searchInput = document.querySelector("#search-input");
+const cityName = document.querySelector(".city-name");
+const weatherState = document.querySelector(".weather-state");
+const temperature = document.querySelector(".temperature");
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
+const humidity = document.querySelector(".humidity");
+const windSpeed = document.querySelector(".wind-speed");
+const DEFAULT_VALUE = "Not found";
+const weatherIcon = document.querySelector(".weather-icon");
 
-  if (searchInput) {
-    searchInput.addEventListener("change", (e) => {
-      fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&lang=vi`
-      ).then(async (res) => {
+if (searchInput) {
+  searchInput.addEventListener("change", (e) => {
+    fetch(
+      `https://api.open123weathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&lang=vi`
+    )
+      .then(async (res) => {
         const data = await res.json();
         console.log("[Search Input]", data);
         cityName.innerHTML = data.name || DEFAULT_VALUE;
@@ -39,9 +41,12 @@ document.addEventListener("DOMContentLoaded", function () {
         humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
         windSpeed.innerHTML =
           (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE; // đổi m/s sang km/h
+      })
+      .catch(function () {
+        console.log("message error");
       });
-    });
-  } else {
-    console.log("Search input element not found");
-  }
-});
+  });
+} else {
+  console.log("Search input element not found");
+}
+// });
